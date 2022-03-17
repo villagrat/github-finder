754d684418f3faf85b3f06e44f94a6188df868d6
villagrat <elsainetedelautrec@gmail.com>
2022-03-17 14:43:57 -0300

    added favicon and Home text

--- src/pages/Home.jsx
+++ src/pages/Home.jsx
@@ -4,6 +4,9 @@ import UserResults from '../components/users/UserResults';
 function Home() {
   return (
     <>
+      <h2 className='mb-4 text-4xl font-light'>
+        Who would you like to search for?
+      </h2>
       <UserSearch />
       <UserResults />
     </>
